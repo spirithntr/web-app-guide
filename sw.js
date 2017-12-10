85196998bead4096fc63fa27032fe113d7bdbafa
Spirit HNTR <spirithntr@gmail.com>
2017-12-10 13:28:58 +0400

    one more try

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@
 
 importScripts('/web-app-guide/sw-toolbox.js');
 
-toolbox.precache(["/web-app-guide/index.html","/web-app-guide/style/style.css", "web-app-guide/intersection.png"]);
+toolbox.precache(["/web-app-guide/index.html","/web-app-guide/style/style.css", "/web-app-guide/intersection.png"]);
 
-toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get('/web-app-guide/images/*', toolbox.cacheFirst);
 
-toolbox.router.get('/*', toolbox.networkFirst, {
+toolbox.router.get('/web-app-guide/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
 });
